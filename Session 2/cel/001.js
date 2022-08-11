6423290a514ffc057edd2da70f15361f29204f21
DULDQ <arian.koochak@gmail.com>
2022-08-11 23:26:08 +0430

    init

--- Session 2/cel/001.js
+++ Session 2/cel/001.js
@@ -1,6 +1,6 @@
 
 function f_to_c(deg){
     deg = (5/9) * (deg - 32)
-    console.log("this degrees on C its " + deg.toFixed(1));
-    alert("this degrees on C its " + deg.toFixed(1));
+    console.log("this degrees on C its " + deg.toFixed(1))
+    alert("this degrees on C its " + deg.toFixed(1))
 }

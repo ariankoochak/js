1c0b46cf7124f1598ee12b51010c5e53af6376eb
arian koochak <arian.koochak@gmail.com>
2022-08-14 20:15:27 +0430

    init on class

--- Session 3/001.js
+++ Session 3/001.js
@@ -1,3 +1,15 @@
+// function OptGenerator() {
+//     let full = "";
+//     for (let i = 0; i < 8; i++) {
+//         let c = Math.floor(Math.random() * 100);
+//         c = Math.round(c/4);
+//         c += 97;
+//         full += String.fromCharCode(c);
+//     }
+//     console.log(full);
+// }
+
+
 function OptGenerator(){
     let full = "";
     for(let i = 0;i < 8;i++){
@@ -9,4 +21,6 @@ function OptGenerator(){
 }
 
 OptGenerator();
+
+
 

e50d3f6f8da3b361bf896a1c83a328dc5308f459
DULDQ <arian.koochak@gmail.com>
2022-08-16 12:12:43 +0430

    init

--- Session 3/Password generator/001.js
+++ Session 3/Password generator/001.js
@@ -1,25 +1,22 @@
-let Full = new Array(8);
-
-
-function NumberGenerator(){
+function NumberGenerator() {
     return String(Math.floor(Math.random() * 10));
 }
 
-function AlphaGenerator(){
+function AlphaGenerator() {
     let c = Math.floor(Math.random() * 26);
     c += 97;
     return String.fromCharCode(c);
 }
 
-function BigAlphaGenerator(){
+function BigAlphaGenerator() {
     let c = Math.floor(Math.random() * 26);
     c += 65;
     return String.fromCharCode(c);
 }
 
-function DiceAndSet(){
+function Dice(){
     let Dice = Math.floor(Math.random() * 3);
-    switch(Dice){
+    switch (Dice) {
         case 0:
             return NumberGenerator();
             break;
@@ -35,29 +32,12 @@ function DiceAndSet(){
     }
 }
 
-function ArrayGenerator(){
-    Full[0] = NumberGenerator();
-    Full[1] = NumberGenerator();
-    Full[2] = AlphaGenerator();
-    Full[3] = AlphaGenerator();
-    Full[4] = BigAlphaGenerator();
-    Full[5] = BigAlphaGenerator();
-    Full[6] = DiceAndSet();
-    Full[7] = DiceAndSet();
-    for (let i = 7; i > 0; i--) {
-        let j = Math.floor(Math.random() * i);
-        let t = Full[i];
-        Full[i] = Full[j];
-        Full[j] = t;
+function SetPassword(){
+    let Password = "";
+    for(let i = 0;i < 8;i++){
+        Password += Dice();
     }
+    return Password;
 }
 
-function PasswordGenerator() {
-    ArrayGenerator();
-    let Password = "";
-    for(let i = 0;i< 8;i++){
-        Password += Full[i];
-    }
-    console.log(Password);
-    alert(Password);
-}
+console.log(SetPassword());

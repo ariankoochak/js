afa5eb1f3c8851f2b2db183b35640f83d5075c8b
DULDQ <arian.koochak@gmail.com>
2022-08-23 00:16:34 +0430

    init

--- Session 4/Change Image/001.js
+++ Session 4/Change Image/001.js
@@ -1,10 +1,9 @@
 let Images = document.querySelectorAll("img");
-let Name = ["Zero","Supra Mk4","Skyline R35","Skyline R34","Lancer Evo 9","Mazda Rx7"]
 let Prev = Images[1];
 function BigFunc(){
     if (this.getAttribute("id") != "big"){
         Images[0].setAttribute("src", this.getAttribute("src"));
-        document.getElementById("name").textContent = Name[Name.indexOf(this)];
+        document.getElementById("name").innerHTML = this.getAttribute("alt");
         Prev.classList.remove("selection");
         this.classList.add("selection");
         Prev = this;

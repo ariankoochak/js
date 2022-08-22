5b3f0dabae51b120ff1668067a3b97f811d91704
arian koochak <arian.koochak@gmail.com>
2022-08-22 08:27:08 +0430

    init

--- Session 4/Change Image/001.js
+++ Session 4/Change Image/001.js
@@ -1,40 +1,13 @@
-let Supra = document.getElementById("supra");
-let R35 = document.querySelector("img:nth-of-type(2)");
-let R34 = document.querySelector("img:nth-of-type(3)");
-let Lancer = document.querySelector("img:nth-of-type(4)");
-let Rx7 = document.querySelector("img:nth-of-type(5)");
-
-let Prev = Supra;
-function DeleteAllBorder(){
-    Supra.classList.remove("selection");
-    R35.classList.remove("selection");
-    R34.classList.remove("selection");
-    Lancer.classList.remove("selection");
-    Rx7.classList.remove("selection");
-}
-function BigFunc(Elem,Name){
-    DeleteAllBorder();
+let Images = document.querySelectorAll("img");
+let Name = ["Zero","Supra Mk4","Skyline R35","Skyline R34","Lancer Evo 9","Mazda Rx7"]
+let Prev = Images[1];
+function BigFunc(){
+    Images[0].setAttribute("src", this.getAttribute("src"));
+    document.getElementById("name").textContent = Name[Name.indexOf(this)];
     Prev.classList.remove("selection");
-    document.getElementById("name").textContent = Name;
-    let NewSrc = Elem.getAttribute("src");
-    Elem.setAttribute("class", "selection");
-    let big = document.getElementById("big");
-    big.setAttribute("src", NewSrc);
-    Prev = Elem;
+    this.classList.add("selection");
+    Prev = this;
 }
-
-Supra.onclick = function() {
-    BigFunc(Supra,"Supra Mk4");
-}
-R35.onclick = function () {
-    BigFunc(R35, "R35");
-}
-R34.onclick = function () {
-    BigFunc(R34, "R34");
-}
-Lancer.onclick = function () {
-    BigFunc(Lancer, "Lancer");
-}
-Rx7.onclick = function () {
-    BigFunc(Rx7, "Rx7");
+for(let Image of Images){
+    Image.onclick = BigFunc;
 }

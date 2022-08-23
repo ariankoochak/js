56a62c142bbd2e9ff7c9d4fb6844627f9e9a1dbc
arian koochak <arian.koochak@gmail.com>
2022-08-23 18:58:43 +0430

    init

--- Session 6/Change Color/main.js
+++ Session 6/Change Color/main.js
@@ -3,8 +3,9 @@ function MakeColor(){
     let Red = Math.floor(Math.random() * 256);
     let Blue = Math.floor(Math.random() * 256);
     let Green = Math.floor(Math.random() * 256);
-    return "background : rgb(" + String(Red) + "," + String(Green) + "," + String(Blue) + ")";
+    return "background : rgb(" + Red + "," + Green + "," + Blue + ")";
 }
+
 function ChangeColor(){
     if(!this.getAttribute("style")){
         this.setAttribute("style" , MakeColor());

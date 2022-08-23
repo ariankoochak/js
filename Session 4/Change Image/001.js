a3a3524ce6e09d663c623575e7ef49e47ae3f809
arian koochak <arian.koochak@gmail.com>
2022-08-23 18:16:47 +0430

    init

--- Session 4/Change Image/001.js
+++ Session 4/Change Image/001.js
@@ -1,12 +1,10 @@
 let Images = document.querySelectorAll("img");
-let Name = ["Zero","Supra Mk4","Skyline R35","Skyline R34","Lancer Evo 9","Mazda Rx7"]
-let Prev = Images[1];
 function BigFunc(){
+    let RemSelc = document.getElementsByClassName("selection");
+    RemSelc[0].classList.remove("selection");
     Images[0].setAttribute("src", this.getAttribute("src"));
-    document.getElementById("name").textContent = Name[Name.indexOf(this)];
-    Prev.classList.remove("selection");
+    document.getElementById("name").textContent = this.getAttribute("alt");
     this.classList.add("selection");
-    Prev = this;
 }
 for(let Image of Images){
     Image.onclick = BigFunc;

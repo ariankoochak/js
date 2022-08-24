976af20234db2b6253ef4ca00a28c65e8ac53c59
DULDQ <arian.koochak@gmail.com>
2022-08-24 11:03:39 +0430

    init

--- Session 6/Random Background/main.js
+++ Session 6/Random Background/main.js
@@ -4,7 +4,6 @@ let Name = ["Kremlin,Moscow",
   "Brandenburg Gate,Berlin",
   "Milad Tower,Tehran",
   "Karoon River,Ahwaz"];
-let Dice = Math.floor(Math.random() * 6);
-Dice++;
+let Dice = Math.floor(Math.random() * 6) + 1;
 let BackSRC = "background-image: url('" + Dice + ".png');";
 document.body.setAttribute("style",BackSRC);

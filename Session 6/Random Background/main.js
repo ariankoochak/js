ed0edfe7e1adaa63f062552fa9ec69776d3adf20
DULDQ <arian.koochak@gmail.com>
2022-08-24 11:06:40 +0430

    init

--- Session 6/Random Background/main.js
+++ Session 6/Random Background/main.js
@@ -7,3 +7,4 @@ let Name = ["Kremlin,Moscow",
 let Dice = Math.floor(Math.random() * 6) + 1;
 let BackSRC = "background-image: url('" + Dice + ".png');";
 document.body.setAttribute("style",BackSRC);
+document.getElementsByTagName("h2")[0].textContent = Name[Dice-1];

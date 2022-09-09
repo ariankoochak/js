40288dea2aabfa6d9f87527d4fdb929e7252de0a
DULDQ <arian.koochak@gmail.com>
2022-09-09 09:18:06 +0430

    init

--- Session 8/ToDoList/main.js
+++ Session 8/ToDoList/main.js
@@ -1,24 +1,29 @@
 let works = []
+let idCounter = 0;
 document.getElementById("submit").onclick = function(){
     let input = document.querySelectorAll("input");
-    let newWork = {
-        id : works.length + 1,
-        title : input[0].value,
-        details : input[1].value
+    if(input[0].value != "" && input[1].value != ""){
+        let newWork = {
+            id : idCounter,
+            title : input[0].value,
+            details : input[1].value
+        }
+        idCounter++;
+        works.push(newWork);
+        document.getElementById("list").innerHTML = "";
+        for(let work of works){
+            let template = `
+            <div class = "${work.id} work">
+                <h3>${work.title}</h3>
+                <p>${work.details}</p>
+                <button class = "${work.id} neo" onclick = "doneButton(${work.id})">Done</button>
+                <button class ="${work.id} neo"  onclick = "removeButton(${work.id})">Delete</button>
+            </div>`;
+            document.getElementById("list").innerHTML += template;
+        }
+        input[0].value = input[1].value = "";
     }
-    works.push(newWork);
-    document.getElementById("list").innerHTML = "";
-    for(let work of works){
-        let template = `
-        <div class = "${work.id}">
-            <h3>${work.title}</h3>
-            <p>${work.details}</p>
-            <button class = "${work.id}" onclick = "doneButton(${work.id})">Done</button>
-            <button class ="${work.id}"  onclick = "removeButton(${work.id})">Delete</button>
-        </div>`;
-        document.getElementById("list").innerHTML += template;
-    }
-    input[0].value = input[1].value = "";
+    console.log(works);
 }
 function doneButton(id){
     let selectId = document.getElementsByClassName(`${id}`);

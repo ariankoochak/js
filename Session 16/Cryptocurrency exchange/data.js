8cf44afde31ca80a063d2097d366757291f7cf79
Arian <arian.koochak@gmail.com>
2022-11-01 09:49:46 +0330

    init-not complete

--- Session 16/Cryptocurrency exchange/data.js
+++ Session 16/Cryptocurrency exchange/data.js
@@ -1,36 +1,42 @@
 PRICES = [
     {
         id : 0,
+        dataSymbol : "btc",
         name : "bitcoin",
         price : "20,563.57",
         img : "./images/bitcoin.png"
     },
     {
         id : 1,
+        dataSymbol : "doge",
         name : "dogecoin",
         price : "0.1181",
         img : "./images/dogcoin.png"
     },
     {
         id : 2,
+        dataSymbol : "eth",
         name : "Ethereum",
         price : "1,585.08",
         img : "./images/Ethereum.png"
     },
     {
         id : 3,
+        dataSymbol : "shib",
         name : "Shiba Inu",
         price : "0.00001208",
         img : "./images/shiba.png"
     },
     {
         id : 4,
+        dataSymbol : "sol",
         name : "Solana",
         price : "33.18",
         img : "./images/solana.png"
     },
     {
         id : 5,
+        dataSymbol : "usdt",
         name : "Tether",
         price : "1.00",
         img : "./images/tether.png"
--- Session 16/Cryptocurrency exchange/main.js
+++ Session 16/Cryptocurrency exchange/main.js
@@ -1,3 +1,19 @@
+
+function splitPrice(price){
+    price = String(price).split("");
+    let splitCounter = 1;
+    for(let i = price.indexOf(".")--;i > 0;i--){
+        if(splitCounter === 3){
+            price.splice(i,0,",");
+            splitCounter = 1;
+        }
+        else
+            splitCounter++;
+    }
+    return price;
+}
+
+
 function render(){
     PRICES.map((crypt) =>{
         const{id , name , price , img} = crypt;
@@ -12,11 +28,14 @@ function render(){
     });
     $("button").on("click",(e)=>{
         let crypto = PRICES.find((crypt) => {
-            if(crypt.id === Number($(e.target).attr("id")))
-                return crypt.price;
-            })
-            $(e.target).siblings()[1].innerHTML = `$${crypto.price}`;
-            $(e.target).html("Update Price");
+            return crypt.id === Number($(e.target).attr("id"))
+        })
+        $.get("https://api.nobitex.ir/market/stats?srcCurrency=btc,eth,etc,usdt,ada,bch,ltc,bnb,eos,xlm,xrp,trx,doge,uni,link,dai,dot,shib,aave,ftm,matic,axs,mana,sand,avax,usdc,gmt,mkr,sol,atom,grt,bat,near,ape,qnt,chz,xmr,gala,busd,algo,pmn&dstCurrency=rls,usdt",(res) => {
+            const {btc , eth ,doge , usdt,sol,shib} = res.global.binance; //bitcoin , etheruem , dogecoin , tether ,solona ,shiba
+            crypto.price = eval(crypto.dataSymbol);
+        })
+        $(e.target).siblings()[1].innerHTML = `$${splitPrice(crypto.price)}`;
+        $(e.target).html("Update Price");
     });
 }
 render();

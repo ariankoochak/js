45b7dda6319ad1f391a29b0cb6eff44b5062d23b
Arian <arian.koochak@protonmail.com>
2022-11-06 19:34:53 +0330

    init

--- Session 18/star wars fetch/main.js
+++ Session 18/star wars fetch/main.js
@@ -2,20 +2,20 @@ let root = document.querySelector("h2");
 async function getPeople(){
     let give = await fetch("https://swapi.dev/api/people/1/");
     give = await give.json();
-    let template = `
-        <h3>name: </h3>
-            <h4>${give.name}</h4><br>
-        <h3>height: </h3>
-            <h4>${give.height}</h4><br>
-        <h3>hair color: </h3>
-            <h4>${give.hair_color}</h4><br>
-        <h3>mass: </h3>
-            <h4>${give.mass}</h4><br>
-        <h3>gender: </h3>
-            <h4>${give.gender}</h4><br>
-    `;
+    let template = makeTemplate(give);
     console.log(give);
     root.innerHTML = template;
+}
+
+function makeTemplate(obj){
+    let template = ``;
+    for (const key in obj) {
+        template += `
+            <h3>${key}: <h3>
+            <h4>${obj[key]}</h4><br><br>
+        `;
+    }
+    return template;
 }
 
 getPeople();

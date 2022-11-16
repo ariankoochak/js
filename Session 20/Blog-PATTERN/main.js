c7efcc738a1e19f62dd225de00595a2b6b6e69ac
Arian <arian.koochak@gmail.com>
2022-11-16 14:31:00 +0330

    init

--- Session 20/Blog-PATTERN/main.js
+++ Session 20/Blog-PATTERN/main.js
@@ -4,20 +4,24 @@
             this.config = cnfg;   
             this.setupTemplate();
             this.setClicks();
+            console.log(history);
         },
         setClicks: function() {
             this.config.renderButton.on("click",this.clickRenderButton);
             this.config.root.on("click", "div", (e) => {this.clickPost(e)});
         },
         clickRenderButton : function(){
+            // history.pushState({}, "Blog", "");
             POSTS.reqAndRender("https://jsonplaceholder.ir/posts");
         },
         clickPost : function(e){
             if($(e.target).data("userid")){
+                // history.pushState({}, "user posts", "");
                 let userID = $(e.target).data("userid");
                 POSTS.reqAndRender(`https://jsonplaceholder.ir/users/${userID}/posts`);
             }
             else{
+                // history.pushState({}, "post", "");
                 let id = $(e.currentTarget).data("id");
                 POSTS.reqAndRender(`https://jsonplaceholder.ir/posts/${id}`)
             }

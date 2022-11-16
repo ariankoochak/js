912c4577db7a167094c9e0d2de823600f9b78b15
Arian <arian.koochak@protonmail.com>
2022-11-16 11:22:59 +0330

    3

--- Session 20/Blog-PATTERN/main.js
+++ Session 20/Blog-PATTERN/main.js
@@ -2,41 +2,46 @@
     let POSTS = {
         doms: function(cnfg) {
             this.config = cnfg;   
-            this.clicks();
             this.setupTemplate();
+            this.setClicks();
         },
-        clicks: function() {
-            this.config.button.on("click",this.clickButton);
-            this.config.root.on("click","div",this.clickPost);
+        setClicks: function() {
+            this.config.allPostsRenderButton.on("click",this.clickAllPostsRenderButton);
+            this.config.root.on("click", "div", (e) => {this.clickPost(e)});
         },
-        clickButton : function(){
-            POSTS.renderOnRoot("https://jsonplaceholder.ir/posts");
+        clickAllPostsRenderButton : function(){
+            POSTS.reqAndRender("https://jsonplaceholder.ir/posts");
         },
-        clickPost : function(){
-            let id = $(this).data("id");
-            POSTS.renderOnRoot(`https://jsonplaceholder.ir/posts/${id}`)
+        clickPost : function(e){
+            if($(e.target).data("userid")){
+                let userID = $(e.target).data("userid");
+                POSTS.reqAndRender(`https://jsonplaceholder.ir/users/${userID}/posts`);
+            }
+            else{
+                let id = $(e.currentTarget).data("id");
+                POSTS.reqAndRender(`https://jsonplaceholder.ir/posts/${id}`)
+            }
         },
         setupTemplate : function(){
             this.template = H.compile(this.config.template.html());
         },
-        renderOnRoot : function(url){
+        reqAndRender : function(url){
             $.ajax({
                 url : url,
                 success : function (res){
-                    if(res[0])
-                        POSTS.config.root.html(POSTS.template({result : res}));
+                    if (res[0])
+                        POSTS.config.root.html(POSTS.template({ result: res }));
                     else
                         POSTS.config.root.html(POSTS.template({ result: [res] }));
                 }
             })
-        }
+        },
     }
 
     POSTS.doms({
-        button : $("button"),
+        allPostsRenderButton : $("#render-button"),
         root : $("#root"),
         template : $("#post"),
-
     })
 
 

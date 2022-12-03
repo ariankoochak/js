81b329df6f71fd0786f21454aef18cd403e44641
Arian <45326611+ariankoochak@users.noreply.github.com>
2022-12-03 10:22:56 +0330

    init

--- Final Project/node/PCmaker/routes/index.js
+++ Final Project/node/PCmaker/routes/index.js
@@ -1,9 +1,40 @@
-var express = require('express');
-var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
 
-module.exports = router;
+// });
+(function(){
+    var express = require('express');
+    var router = express.Router();
+    var mysql = require('mysql');
+
+
+    let API={
+        urls: function(cnfg){
+            for(url in cnfg){
+                //url => apple
+                //cnfg[url] => hello
+            }
+        },
+        all: function(url,mehtod){
+            switch(mehtod){
+                case "GET":
+                    router.get(url, function (req, res, next) {
+                        res.send("its work");
+                    });
+                break;
+                case "POST":
+                    router.post(url, function (req, res, next) {
+                        res.send("its work");
+                    });
+                    break;
+            }
+        },
+    }
+    
+    API.urls({
+        mainRoot : '/',
+        cpu : '/cpu',
+    })
+    module.exports = router;
+})();

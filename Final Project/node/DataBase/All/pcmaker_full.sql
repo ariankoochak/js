b097de34316ca6706bf706861537e43bdfef7f8f
Arian <45326611+ariankoochak@users.noreply.github.com>
2022-12-22 01:43:00 +0330

    fix database

--- Final Project/node/DataBase/All/pcmaker_full.sql
+++ Final Project/node/DataBase/All/pcmaker_full.sql
@@ -183,7 +183,7 @@ INSERT INTO `pccpu` (`id`, `en_name`, `fa_name`, `company`, `cpu_socket`, `haveG
 	(1,'AMD Ryzen7 Pro 5750G','پردازنده ی ای ام دی مدل Ryzen7 5750G','AMD','AM4',1,24637,'CPU/Ryzen7-Pro5750G-Show.png','CPU/Ryzen7-Pro5750G-Show.png','7,780,000'),
 	(2,'AMD Ryzen5 5600X','پردازنده ی ای ام دی مدل Ryzen5 5600X','AMD','AM4',1,21950,'CPU/Ryzen5-5600X-Show.png','CPU/Ryzen5-5600X-Small.png','7,690,000'),
 	(3,'Intel Core i5 9400f','پردازنده ی اینتل مدل core i5 9400f','Intel','LGA-1151',0,9540,'CPU/i5-9400f-Show.png','CPU/i5-9400f-Small.png','4,597,000'),
-	(4,'Intel Core i3 9100','پردازنده ی اینتل مدل core i3 9100','Intel','LGA-1151',1,6663,'CPU/i3-9100-Show.png','CPU/i5-9100-Small.png','4,478,000'),
+	(4,'Intel Core i3 9100','پردازنده ی اینتل مدل core i3 9100','Intel','LGA-1151',1,6663,'CPU/i3-9100-Show.png','CPU/i3-9100-Small.png','4,478,000'),
 	(5,'Intel Core i5 10400f','پردازنده ی اینتل مدل core i5 10400f','Intel','LGA-1200',1,12369,'CPU/i5-10400f-Show.png','CPU/i5-10400f-Small.png','3,778,000');
 UNLOCK TABLES;
 
